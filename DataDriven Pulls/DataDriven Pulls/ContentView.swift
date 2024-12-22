c715bcd8799b2242af09a54c7578f16b7dd873fe
tyler <124809459+Tyraney89@users.noreply.github.com>
2024-12-22 14:47:30 -0600

    Skeleton Project

---
+++ DataDriven Pulls/DataDriven Pulls/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DataDriven Pulls
+//
+//  Created by Tyler Burke on 12/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DataDriven Pulls/DataDriven Pulls/DataDriven_PullsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataDriven_PullsApp.swift
+//  DataDriven Pulls
+//
+//  Created by Tyler Burke on 12/22/24.
+//
+
+import SwiftUI
+
+@main
+struct DataDriven_PullsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
