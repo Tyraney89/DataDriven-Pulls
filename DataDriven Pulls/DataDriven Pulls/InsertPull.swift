230a3e3ee03469bad779a6090ff63668b83525e7
tyler <124809459+Tyraney89@users.noreply.github.com>
2024-12-23 13:23:33 -0600

    fixed some issues

--- DataDriven Pulls/DataDriven Pulls/InsertPull.swift
+++ DataDriven Pulls/DataDriven Pulls/InsertPull.swift
@@ -6,43 +6,24 @@
 //
 
 import SwiftUI
+import MapKit
+import CoreLocation
 
 struct InsertPull: View {
-    //Pull Details
-    @State private var puller: String = ""
-    @State private var tractor: String = ""
-    @State private var distance: String = ""
-    @State private var sled: String = ""
-    @State private var tclass: String = ""
-    
-    //Tractor Details
-    @State private var tirePressure: String = ""
-    @State private var gear: String = ""
-    @State private var frontWeight: String = ""
-    @State private var bellyWeight: String = ""
-    @State private var backWeight: String = ""
-    
+    //Pull details
+    @State private var pullName: String = ""
+    @State private var pullDate = Date()
+    @State private var pullLocation: String = ""
     var body: some View {
         NavigationView{
             VStack{
                 Form{
                     Section(header: Text("Pull Details")){
-                        TextField("Puller (e.g., Rick)", text: $puller)
-                        TextField("Tractor (e.g., WhyNot)", text: $tractor)
-                        TextField("Class (e.g., 9500 Pro Farm)", text: $tclass)
-                        TextField("Distance (e.g., 300 ft)", text: $distance).keyboardType(.decimalPad)
-                        TextField("Sled (e.g., Red Rock)", text: $sled)
+                        TextField("Pull Name (e.g., Cow Town)", text: $pullName)
+                        DatePicker("Pull Date", selection: $pullDate, displayedComponents: .date)
+                        TextField("Pull Location (e.g., Kansas City MO)", text: $pullLocation)
                     }
-                    
-                    
-                    Section(header: Text("Tractor Details")){
-                        TextField("Tire Pressure (e.g., 25.5 psi)", text: $tirePressure)
-                        TextField("Gear (e.g., 3rd)", text: $gear)
-                        TextField("Front Weight (e.g., 300 lbs)", text: $frontWeight)
-                        TextField("Belly Weight (e.g., 300 lbs", text: $bellyWeight)
-                        TextField("Back Weight (e.g., 300 lbs)", text: $backWeight)
-                    }
-                }.navigationTitle("Record Pull")
+                }.navigationTitle("Record a New Pull")
                 
             }
         }
