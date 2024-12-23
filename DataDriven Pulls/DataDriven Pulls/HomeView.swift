79ab8e8beca5924640c6b79000aeeb329701f70a
tyler <124809459+Tyraney89@users.noreply.github.com>
2024-12-22 21:26:15 -0600

    HomeView done until data

--- DataDriven Pulls/DataDriven Pulls/HomeView.swift
+++ DataDriven Pulls/DataDriven Pulls/HomeView.swift
@@ -9,15 +9,32 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        NavigationView{
-            List{
-                PullRow(Location: "New Market", Class: "9500 Pro farm", Date: "11-11-02")
-                PullRow(Location: "Burlington Junciton", Class: "Pro Stock", Date: "11-11-02")
-                PullRow(Location: "St Joeseph", Class: "10mph clas", Date: "11-11-02")
-                PullRow(Location: "Mexico", Class: "Pro Stock", Date: "11-11-02")
+        TabView{
+            NavigationView{
+                ScrollView{
+                    LazyVStack{
+                        PullRow(Location: "New Market", Class: "9500 Pro farm", Date: "11-11-02")
+                        PullRow(Location: "Burlington Junciton", Class: "Pro Stock", Date: "11-11-02")
+                        PullRow(Location: "St Joeseph", Class: "10mph clas", Date: "11-11-02")
+                        PullRow(Location: "Mexico", Class: "Pro Stock", Date: "11-11-02")
+                        PullRow(Location: "St Joeseph", Class: "10mph clas", Date: "11-11-02")
+                        PullRow(Location: "Mexico", Class: "Pro Stock", Date: "11-11-02")
+                        PullRow(Location: "St Joeseph", Class: "10mph clas", Date: "11-11-02")
+                        PullRow(Location: "Mexico", Class: "Pro Stock", Date: "11-11-02")
+                        PullRow(Location: "St Joeseph", Class: "10mph clas", Date: "11-11-02")
+                        PullRow(Location: "Mexico", Class: "Pro Stock", Date: "11-11-02")
+                    }
+                    .navigationTitle("Recent Pulls")
+                }
+            }.tabItem {
+                Image(systemName: "house")
+                Text("Home")
             }
-            .navigationTitle("Recent Pulls")
-        }
+            InsertPull().tabItem {
+                Image(systemName: "plus.app")
+                Text("Add Pull")
+            }
+        }.accentColor(Color("PullingColor"))
     }
 }
 
@@ -41,11 +58,11 @@ struct PullRow: View {
                 }
                 Spacer()
                 VStack{
-                    Text(Date).font(.subheadline).foregroundColor(.blue)
+                    Text(Date).font(.subheadline).foregroundColor(Color("PullingColor"))
                 }
             }
             .padding() // Padding inside the bubble
-            .background(Color.blue.opacity(0.1)) // Background color for the bubble
+            .background(Color("PullingColor").opacity(0.1)) // Background color for the bubble
             .cornerRadius(15) // Rounded corners for the bubble
             .shadow(radius: 5) // Shadow to give it a lifted effect
             .padding(.horizontal) // Padding on the sides of the bubble
