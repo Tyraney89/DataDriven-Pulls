ffb485a3c55507087dbde9176a6b030da0b618bb
tyler <124809459+Tyraney89@users.noreply.github.com>
2024-12-22 22:14:46 -0600

    skeleton done for Details

---
+++ DataDriven Pulls/DataDriven Pulls/PullingDetailsView.swift
@@ -0,0 +1,68 @@
+//
+//  PullingDetailsView.swift
+//  DataDriven Pulls
+//
+//  Created by Tyler Burke on 12/22/24.
+//
+
+import SwiftUI
+
+struct PullingDetailsView: View {
+    var body: some View {
+        NavigationView{
+            VStack{
+                HStack{
+                    VStack(alignment: .leading){
+                        Text("Date: 11-11-02")
+                        Text("Location: New Market")
+                    }
+                    Spacer()
+                }.padding()
+                NavigationView{
+                    ScrollView{
+                        LazyVStack{
+                            Hooks(Puller: "Rick", Class: "9500 Pro Farm", Distance: "300 ft", Place: "1st")
+                            Hooks(Puller: "Jerrica", Class: "9500 Pro Farm", Distance: "300 ft", Place: "1st")
+                        }
+                        .navigationTitle("Hooks")
+                    }
+                }
+            }.navigationTitle("Pull Name")
+        }
+    }
+}
+
+#Preview {
+    PullingDetailsView()
+}
+
+struct Hooks: View {
+    var Puller: String
+    var Class: String
+    var Distance: String
+    var Place: String
+    var body: some View {
+        VStack{
+            HStack{
+                VStack(alignment: .leading)
+                {
+                    Text(Puller).font(.headline)
+                    Spacer()
+                    Text(Class).font(.subheadline).foregroundColor(.gray)
+                }
+                Spacer()
+                VStack{
+                    Text(Distance).font(.subheadline).foregroundColor(Color("PullingColor"))
+                    Text(Place).font(.subheadline).foregroundColor(Color("PullingColor"))
+                }
+            }
+            .padding() // Padding inside the bubble
+            .background(Color("PullingColor").opacity(0.1)) // Background color for the bubble
+            .cornerRadius(15) // Rounded corners for the bubble
+            .shadow(radius: 5) // Shadow to give it a lifted effect
+            .padding(.horizontal) // Padding on the sides of the bubble
+        }
+        .padding(.vertical, 8)
+        .listRowInsets(EdgeInsets())
+    }
+}
